57828a6b99faf0a635be9eff2dd6cea6a04216c9
Samir Oliveira <samir.oliveira@gmail.com>
2016-10-22 12:08:14 -0200

    Include PDF split helper

--- PresentationBuilder/Helpers/PdfHelper.cs
+++ PresentationBuilder/Helpers/PdfHelper.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GhostscriptSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,41 @@ namespace PresentationBuilder.Helpers
 {
     public class PdfHelper
     {
-        public static bool slpit()
+
+        /// <summary>
+        /// Split PDF file into images with File pattern name OUTPUT{0}.jpg Total Page to export to image = 100;
+        /// </summary>
+        /// <param name="path">PDF path source</param>
+        /// <param name="output">Images PDF output</param>
+        /// <returns></returns>
+        public static void splitToImages(string path, string outputPath)
         {
-            return false;
+            splitToImages(path, outputPath, "OUTPUT%d.jpg", 100);
+        }
+
+
+        /// <summary>
+        /// Split PDF file into images
+        /// </summary>
+        /// <param name="path">PDF path source</param>
+        /// <param name="output">Images PDF output</param>
+        /// <param name="filename">File pattern name OUTPUT{0}.jpg</param>
+        /// <param name="multipleFilePageCount">Total Page to export to image</param>
+        /// <returns></returns>
+        public static void splitToImages(string path, string outputPath, string filename = "OUTPUT%d.jpg", int multipleFilePageCount = 100)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);                
+
+                GhostscriptWrapper.GeneratePageThumbs(path, Path.Combine(outputPath, filename), 1, multipleFilePageCount, 100, 100);
+            }
+            catch (Exception exc)
+            {
+               
+                //throw;
+            }
         }
     }
 }
