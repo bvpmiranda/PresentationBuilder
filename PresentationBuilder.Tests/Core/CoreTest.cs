3b38181c22bd54a5be72f0805c202e77bb86442c
Samir Oliveira <samir.oliveira@gmail.com>
2016-10-22 15:09:08 -0200

    PDF Core test

--- PresentationBuilder.Tests/Core/CoreTest.cs
+++ PresentationBuilder.Tests/Core/CoreTest.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using PresentationBuilder.Core.Util;
 using GhostscriptSharp;
 using System.IO;
 
@@ -12,44 +11,64 @@ namespace PracticalDeveloperTest.Tests
     [TestClass]
     public class UnitTestRequirements
     {
-        [TestMethod]
-        public void Example4()
-        {
-            PDF.splitPDF1();
-           
-            //Assert.IsTrue(valueTotal == 12.50M, "The sale total is wrong  - " + valueTotal.ToString());
-        }
-
 
       
     }
 
     [TestClass]
-    public class GhostscriptSharpTests
+    public class ZipHelperTests
     {
-        //string inputPdfPath = @"E:\Pessoal\Livros\Armazenando dados com Redis - Casa do Codigo.pdf";
-
-        //ExtractImagesFromPDF(inputPdfPath, @"E:\Pessoal\Livros\img\");
 
         private readonly string TEST_FILE_LOCATION = @"E:\Pessoal\Livros\Armazenando dados com Redis - Casa do Codigo.pdf";
         private readonly string SINGLE_FILE_LOCATION = @"E:\Pessoal\Livros\img\output.jpg";
         private readonly string MULTIPLE_FILE_LOCATION = @"E:\Pessoal\Livros\img\output%d.jpg";
+        private readonly string OUTPUT_FOLDER = @"E:\Pessoal\Livros\img\";
 
-        private readonly int MULTIPLE_FILE_PAGE_COUNT = 20;
+        private readonly int MULTIPLE_FILE_PAGE_COUNT = 2000;
 
         [TestMethod]
         public void GenerateSinglePageThumbnail()
         {
-            GhostscriptWrapper.GeneratePageThumb(TEST_FILE_LOCATION, SINGLE_FILE_LOCATION, 1, 100, 100);
-            Assert.IsTrue(File.Exists(SINGLE_FILE_LOCATION));
+            //PresentationBuilder.Helpers.PdfHelper.splitToImages(TEST_FILE_LOCATION, OUTPUT_FOLDER);
+
+
+            //for (var i = 1; i <= MULTIPLE_FILE_PAGE_COUNT; i++)
+            //    Assert.IsTrue(File.Exists(String.Format("output{0}.jpg", i)));
+
+
+            //GhostscriptWrapper.GeneratePageThumb(TEST_FILE_LOCATION, SINGLE_FILE_LOCATION, 1, 100, 100);
+            //Assert.IsTrue(File.Exists(SINGLE_FILE_LOCATION));
         }
 
         [TestMethod]
         public void GenerateMultiplePageThumbnails()
         {
+           int iTotal = GetNumPages(TEST_FILE_LOCATION);
             GhostscriptWrapper.GeneratePageThumbs(TEST_FILE_LOCATION, MULTIPLE_FILE_LOCATION, 1, MULTIPLE_FILE_PAGE_COUNT, 100, 100);
             for (var i = 1; i <= MULTIPLE_FILE_PAGE_COUNT; i++)
                 Assert.IsTrue(File.Exists(String.Format("output{0}.jpg", i)));
+        }
+
+        public static int GetNumPages(string path)
+        {
+            if (path != null)
+            {
+                try
+                {
+                    using (var stream = new StreamReader(File.OpenRead(path)))
+                    {
+                        var regex = new System.Text.RegularExpressions.Regex(@"/Type\s*/Page[^s]");
+                        var matches = regex.Matches(stream.ReadToEnd());
+
+                        return matches.Count;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return 0;
+                }
+            }
+            return 0;
         }
 
     }
