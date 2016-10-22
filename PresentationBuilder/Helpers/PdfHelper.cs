81f99b69d88cd0cca77607aef908e013845de61b
Rafael Passarela <rafaelpassarela@gmail.com>
2016-10-22 02:48:58 -0200

    PdfSharp

--- PresentationBuilder/Helpers/PdfHelper.cs
+++ PresentationBuilder/Helpers/PdfHelper.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 
 namespace PresentationBuilder.Helpers
 {
@@ -9,6 +12,21 @@ namespace PresentationBuilder.Helpers
     {
         public static bool slpit()
         {
+            var filename = "";
+            PdfDocument inputDocument = PdfReader.Open(filename, PdfDocumentOpenMode.ReadOnly);
+            //inputDocument.PageCount
+
+            string path = HttpContext.Current.Server.MapPath("~/");
+
+            if (path.EndsWith("\\"))
+            {
+                path = path.Substring(0, path.Length - 1);
+            }
+
+            path = path.Substring(0, path.LastIndexOf("\\")) + "\\PresentationBuilderDocuments\\";
+
+            var pdfName = path + "Teste.pdf";
+
             return false;
         }
     }
