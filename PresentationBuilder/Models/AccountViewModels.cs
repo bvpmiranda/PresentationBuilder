454a9609163048899c51276cbb634859d259e255
Rafael Passarela <rafaelpassarela@gmail.com>
2016-10-22 19:04:21 -0200

    Login with Google+ (get user info)

--- PresentationBuilder/Models/AccountViewModels.cs
+++ PresentationBuilder/Models/AccountViewModels.cs
@@ -6,6 +6,10 @@ namespace PresentationBuilder.Models
     public class ExternalLoginConfirmationViewModel
     {
         [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
