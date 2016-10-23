cb39af367fa514d1f5138c33c5a918ae42b614bd
Samir Oliveira <samir.oliveira@gmail.com>
2016-10-23 02:58:12 -0200

    Changed Dropzone css;  Duplicate method; Reorder list included.

--- PresentationBuilder/Controllers/PresentationsController.cs
+++ PresentationBuilder/Controllers/PresentationsController.cs
@@ -9,137 +9,168 @@ using System.Web.Mvc;
 
 namespace PresentationBuilder.Controllers
 {
-	public class PresentationsController : Controller
-	{
-		[Authorize]
-		public ActionResult Index()
-		{
-			var context = new PresentationBuilderEntities();
+    public class PresentationsController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            var context = new PresentationBuilderEntities();
 
-			var presentations = (from p in context.Presentations.Include("AspNetUser")
-								 where p.AspNetUser.UserName == User.Identity.Name
-								 orderby p.Name
-								 select p).ToList();
+            var presentations = (from p in context.Presentations.Include("AspNetUser")
+                                 where p.AspNetUser.UserName == User.Identity.Name
+                                 orderby p.Name
+                                 select p).ToList();
 
-			return View(presentations);
-		}
+            return View(presentations);
+        }
 
-		[Authorize]
-		public ActionResult Presentation(int id)
-		{
-			var context = new PresentationBuilderEntities();
+        [Authorize]
+        public ActionResult Presentation(int id)
+        {
+            var context = new PresentationBuilderEntities();
 
-			var presentation = (from p in context.Presentations.Include("PresentationPages") where p.PresentationId == id select p).First();
-
-
-			return View(presentation);
-		}
-
-		[Authorize]
-		[HttpPost]
-		public ActionResult UploadZipAsync()
-		{
-			var uploadReturn = new UploadReturn();
-
-			var context = new PresentationBuilderEntities();
-
-			var UserId = (from u in context.AspNetUsers where u.UserName == User.Identity.Name select u.Id).First();
-
-			try
-			{
-				var Presentation = Helpers.ZipHelper.unzipPresentation(Request.Files[0], UserId);
-
-				uploadReturn.data = new Presentation
-				{
-					PresentationId = Presentation.PresentationId,
-					Name = Presentation.Name
-				};
-			}
-			catch (Exception ex)
-			{
-				uploadReturn.uploadStatus = uploadStatus.Error;
-				uploadReturn.message = ex.Message;
-			}
-
-			return Json(uploadReturn, "text/plain");
-		}
-
-		[Authorize]
-		[HttpPost]
-		public ActionResult SaveUploadedFile()
-		{
-			var uploadReturn = new UploadReturn();
-
-			string fName = "";
-
-			try
-			{
-				foreach (string fileName in Request.Files)
-				{
-					HttpPostedFileBase file = Request.Files[fileName];
-					//Save file content goes here
-					fName = file.FileName;
-					if (file != null && file.ContentLength > 0)
-					{
-						var fileNameUpload = Path.GetFileName(file.FileName);
-
-						//Create Presentation
-						var context = new PresentationBuilderEntities();
-
-						var UserId = (from u in context.AspNetUsers where u.UserName == User.Identity.Name select u.Id).First();
-
-						Presentation presentation = new Presentation
-						{
-							UserId = UserId,
-							Date = DateTime.Now,
-							Name = "New Presentation",
-							Description = null
-						};
-
-						context.Presentations.Add(presentation);
-						context.SaveChanges();
-
-						uploadReturn.data = presentation;
-
-						string pathWork = Path.Combine(ZipHelper.path(), presentation.PresentationId.ToString());
-
-						if (!Directory.Exists(pathWork))
-							Directory.CreateDirectory(pathWork);
+            var presentation = (from p in context.Presentations.Include("PresentationPages") where p.PresentationId == id select p).First();
 
 
+            return View(presentation);
+        }
 
-						file.SaveAs(Path.Combine(pathWork, file.FileName));
+        [Authorize]
+        [HttpPost]
+        public ActionResult UploadZipAsync()
+        {
+            var uploadReturn = new UploadReturn();
 
-						PdfHelper.splitToImages(Path.Combine(pathWork, file.FileName), pathWork);
+            var context = new PresentationBuilderEntities();
+
+            var UserId = (from u in context.AspNetUsers where u.UserName == User.Identity.Name select u.Id).First();
+
+            try
+            {
+                var Presentation = Helpers.ZipHelper.unzipPresentation(Request.Files[0], UserId);
+
+                uploadReturn.data = new Presentation
+                {
+                    PresentationId = Presentation.PresentationId,
+                    Name = Presentation.Name
+                };
+            }
+            catch (Exception ex)
+            {
+                uploadReturn.uploadStatus = uploadStatus.Error;
+                uploadReturn.message = ex.Message;
+            }
+
+            return Json(uploadReturn, "text/plain");
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult SaveUploadedFile()
+        {
+            var uploadReturn = new UploadReturn();
+
+            string fName = "";
+
+            try
+            {
+                foreach (string fileName in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[fileName];
+                    //Save file content goes here
+                    fName = file.FileName;
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileNameUpload = Path.GetFileName(file.FileName);
+
+                        //Create Presentation
+                        var context = new PresentationBuilderEntities();
+
+                        var UserId = (from u in context.AspNetUsers where u.UserName == User.Identity.Name select u.Id).First();
+
+                        Presentation presentation = new Presentation
+                        {
+                            UserId = UserId,
+                            Date = DateTime.Now,
+                            Name = "New Presentation",
+                            Description = null
+                        };
+
+                        context.Presentations.Add(presentation);
+                        context.SaveChanges();
+
+                        uploadReturn.data = presentation;
+
+                        string pathWork = Path.Combine(ZipHelper.path(), presentation.PresentationId.ToString());
+
+                        if (!Directory.Exists(pathWork))
+                            Directory.CreateDirectory(pathWork);
 
 
-						byte bOrder = 0;
 
-						foreach (string filePresentaion in Directory.EnumerateFiles(pathWork, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".jpg")))
-						{
-							presentation.PresentationPages.Add(new PresentationPage
-							{
-								Order = bOrder++,
-								ImagePath = Path.GetFileName(filePresentaion),
-								SoundPath = null,
-								SoundLength = null
-							});
-						}
+                        file.SaveAs(Path.Combine(pathWork, file.FileName));
 
-						context.SaveChanges();
-
-						return Json(new { Message = presentation.PresentationId });
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				uploadReturn.uploadStatus = uploadStatus.Error;
-				uploadReturn.message = ex.Message;
-			}
+                        PdfHelper.splitToImages(Path.Combine(pathWork, file.FileName), pathWork);
 
 
-			return Json(uploadReturn, "text/plain");
-		}
-	}
+                        byte bOrder = 0;
+
+                        foreach (string filePresentaion in Directory.EnumerateFiles(pathWork, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".jpg")))
+                        {
+                            presentation.PresentationPages.Add(new PresentationPage
+                            {
+                                Order = bOrder++,
+                                ImagePath = Path.GetFileName(filePresentaion),
+                                SoundPath = null,
+                                SoundLength = null
+                            });
+                        }
+
+                        context.SaveChanges();
+
+                        return Json(new { Message = presentation.PresentationId });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                uploadReturn.uploadStatus = uploadStatus.Error;
+                uploadReturn.message = ex.Message;
+            }
+
+
+            return Json(uploadReturn, "text/plain");
+        }
+
+        public void DuplicatePresentation(int id)
+        {
+
+            var context = new PresentationBuilderEntities();
+
+            var currentPresentation = (from p in context.Presentations.Include("PresentationPages") where p.PresentationId == id select p).First();
+
+            var newPresentation = currentPresentation;
+
+            newPresentation.PresentationId = 0;
+
+            context.Entry(newPresentation).State = System.Data.Entity.EntityState.Unchanged;
+
+            context.Presentations.Add(newPresentation);
+
+            context.SaveChanges();
+
+            foreach (var oPresentationPage in currentPresentation.PresentationPages)
+            {
+                var newPresentationPage = oPresentationPage;
+
+                newPresentationPage.PresentationId = newPresentation.PresentationId;
+
+                context.Entry(newPresentationPage).State = System.Data.Entity.EntityState.Unchanged;
+
+                context.PresentationPages.Add(newPresentationPage);
+            }
+
+            context.SaveChanges();
+        }
+    }
 }
---
+++ PresentationBuilder/Core/Util/PDF.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Drawing.Imaging;
+using System.Collections;
+using System.Drawing;
+using iTextSharp.text.pdf;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+
+namespace PresentationBuilder.Core.Util
+{
+    public static class PDF
+    {
+
+        
+        //public class TiffImage
+        //{
+        //    private string myPath;
+        //    private Guid myGuid;
+        //    private FrameDimension myDimension;
+        //    public ArrayList myImages = new ArrayList();
+        //    private int myPageCount;
+        //    private Bitmap myBMP;
+
+        //    public TiffImage(string path)
+        //    {
+        //        MemoryStream ms;
+        //        Image myImage;
+
+        //        myPath = path;
+        //        using (FileStream fs = new FileStream(myPath, FileMode.Open))
+        //        {
+        //            Stream oStream = new System.IO.File( @"C:\MyFile.pdf", "application/pdf");
+        //            myImage = Image.FromStream(fs as Stream);
+        //            myGuid = myImage.FrameDimensionsList[0];
+        //            myDimension = new FrameDimension(myGuid);
+        //            myPageCount = myImage.GetFrameCount(myDimension);
+        //            for (int i = 0; i < myPageCount; i++)
+        //            {
+        //                ms = new MemoryStream();
+        //                myImage.SelectActiveFrame(myDimension, i);
+        //                myImage.Save(ms, ImageFormat.Bmp);
+        //                myBMP = new Bitmap(ms);
+        //                myImages.Add(myBMP);
+        //                ms.Close();
+        //            }
+        //        }
+        //    }
+        //}
+
+        public static void ExtractImagesFromPDF(string sourcePdf, string outputPath)
+        {
+            // NOTE:  This will only get the first image it finds per page.
+            PdfReader pdf = new PdfReader(sourcePdf);
+            RandomAccessFileOrArray raf = new iTextSharp.text.pdf.RandomAccessFileOrArray(sourcePdf);
+
+            try
+            {
+                for (int pageNumber = 1; pageNumber <= pdf.NumberOfPages; pageNumber++)
+                {
+
+
+                    PdfDictionary pg = pdf.GetPageN(pageNumber);
+
+                    // recursively search pages, forms and groups for images.
+                    PdfObject obj = FindImageInPDFDictionary(pg);
+                    if (obj != null)
+                    {
+
+                        int XrefIndex = Convert.ToInt32(((PRIndirectReference)obj).Number.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                        PdfObject pdfObj = pdf.GetPdfObject(XrefIndex);
+                        PdfStream pdfStrem = (PdfStream)pdfObj;
+                        byte[] bytes = PdfReader.GetStreamBytesRaw((PRStream)pdfStrem);
+                        if ((bytes != null))
+                        {
+                            using (System.IO.MemoryStream memStream = new System.IO.MemoryStream(bytes))
+                            {
+                                memStream.Position = 0;
+                                System.Drawing.Image img = System.Drawing.Image.FromStream(memStream);
+                                // must save the file while stream is open.
+                                if (!Directory.Exists(outputPath))
+                                    Directory.CreateDirectory(outputPath);
+
+                                string path = Path.Combine(outputPath, String.Format(@"{0}.jpg", pageNumber));
+                                System.Drawing.Imaging.EncoderParameters parms = new System.Drawing.Imaging.EncoderParameters(1);
+                                parms.Param[0] = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Compression, 0);
+                                System.Drawing.Imaging.ImageCodecInfo jpegEncoder = GetEncoder(ImageFormat.Jpeg);// Utilities.GetImageEncoder("JPEG");
+                                img.Save(path, jpegEncoder, parms);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                pdf.Close();
+                raf.Close();
+            }
+
+
+        }
+
+        private static PdfObject FindImageInPDFDictionary(PdfDictionary pg)
+        {
+            PdfDictionary res =
+                (PdfDictionary)PdfReader.GetPdfObject(pg.Get(PdfName.RESOURCES));
+
+
+            PdfDictionary xobj =
+              (PdfDictionary)PdfReader.GetPdfObject(res.Get(PdfName.XOBJECT));
+            if (xobj != null)
+            {
+                foreach (PdfName name in xobj.Keys)
+                {
+
+                    PdfObject obj = xobj.Get(name);
+                    if (obj.IsIndirect())
+                    {
+                        PdfDictionary tg = (PdfDictionary)PdfReader.GetPdfObject(obj);
+
+                        PdfName type =
+                          (PdfName)PdfReader.GetPdfObject(tg.Get(PdfName.SUBTYPE));
+
+                        //image at the root of the pdf
+                        if (PdfName.IMAGE.Equals(type))
+                        {
+                            return obj;
+                        }// image inside a form
+                        else if (PdfName.FORM.Equals(type))
+                        {
+                            return FindImageInPDFDictionary(tg);
+                        } //image inside a group
+                        else if (PdfName.GROUP.Equals(type))
+                        {
+                            return FindImageInPDFDictionary(tg);
+                        }
+
+                    }
+                }
+            }
+
+            return null;
+
+        }
+
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        public static  void splitPDF1()
+        {
+            //string inputPdfPath = @"E:\Pessoal\Livros\Armazenando dados com Redis - Casa do Codigo.pdf";
+
+            //ExtractImagesFromPDF(inputPdfPath, @"E:\Pessoal\Livros\img\");
+
+
+            //int desired_x_dpi = 96;
+            //int desired_y_dpi = 96;
+
+            //string inputPdfPath = @"E:\Pessoal\Livros\1.pdf";
+            //TiffImage oTiffImage = new TiffImage(inputPdfPath);
+
+            //string outputDirectory = Environment.CurrentDirectory;
+            ////ImageMagickNET.
+            //try
+            //{
+
+            //    using (ImageList imageList = new ImageList())
+            //    {
+            //        imageList.ReadImages(inputPdfPath);
+            //        int pageIndex = 0;
+            //        foreach (Image page in imageList)
+            //        {
+            //            page.Write(outputDirectory + "\\Page." + pageIndex + ".jpg");
+            //            pageIndex++;
+            //        }
+            //    }
+
+            //    //using (Image firstPage = new Image())
+            //    //{
+            //    //    firstPage.Read(inputFile + "[0]");
+            //    //    firstPage.Write(outputDirectory + "\\FirstPage.jpg");
+            //    //}
+            //    //_rasterizer.Close();
+            //}
+            //catch (Exception)
+            //{
+                
+            //    throw;
+            //}
+          
+        }
+    }
+}
